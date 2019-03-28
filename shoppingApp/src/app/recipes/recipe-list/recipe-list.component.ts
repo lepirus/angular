de103ff705adbc43c30986dc0cd647f64b56d0b4
lepirus <lepirus@gmail.com>
2019-03-28 16:41:13 +0100

    3.48 Adding Content to the Recipes Components

--- shoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple test description',
+      'https://pxhere.com/en/photo/604134')
+  ];
 
   constructor() { }
 
