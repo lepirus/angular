94ee42a45460dc90f4f6a3217b3450ed79dbfe8e
lepirus <lepirus@gmail.com>
2019-03-29 09:39:55 +0100

    5.62 Binding to Custom Properties. Communicate data between components

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+  // Type definition for a property
+  // @Input(): to make it public to AppComponent, which uses it in app.component.html, [element]="serverElement"
+  @Input() element: {type: string, name: string, content: string};
 
   constructor() { }
 
