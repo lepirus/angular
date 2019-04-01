40cf4aace8169d2bdd71d2b6f2a3da34ec1946c2
lepirus <lepirus@gmail.com>
2019-04-01 13:12:36 +0200

    Assignment 4: Practicing Property & Event Binding and View Encapsulation. 6 and 7 steps

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,9 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
 
   onEventFired(firedNumber: number) {
     console.log('onEventFired method!');
     console.log('firedNumber: ' + firedNumber);
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
   }
 }
