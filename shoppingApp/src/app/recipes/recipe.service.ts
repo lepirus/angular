25f8d7d67a0469e1bdfb09778f66734b6fe8403e
lepirus <lepirus@gmail.com>
2019-04-03 22:41:57 +0200

    10.112 Adding Ingredients to Recipes

--- shoppingApp/src/app/recipes/recipe.service.ts
+++ shoppingApp/src/app/recipes/recipe.service.ts
@@ -1,14 +1,28 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simple test description',
-      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
-    new Recipe('A Test Recipe2', 'Test description 2',
-      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d')
+    new Recipe('Tortilla de Patatas',
+      'A tasty tortilla',
+      'https://upload.wikimedia.org/wikipedia/commons/3/3c/Tortilla_de_patatas_-_2009.jpg',
+      [
+        new Ingredient('Potatoes', 10),
+        new Ingredient('Augnions', 1),
+        new Ingredient('Eggs', 6)
+      ]),
+    new Recipe('Fat Big Burguer',
+      'The Fat Big Burguer as you ever can eat',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Bacon', 2),
+        new Ingredient('Cheese', 2)
+      ])
   ];
 
   getRecipes() {
