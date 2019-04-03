fbb26e7db3de5e2f03029fd973a0c5f5badef4c5
lepirus <lepirus@gmail.com>
2019-04-03 13:31:33 +0200

    10.108 Managing Recipes in a Recipe Service

--- shoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,16 +9,12 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simple test description',
-      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
-    new Recipe('A Test Recipe2', 'Test description 2',
-      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d')
-  ];
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- shoppingApp/src/app/recipes/recipe.service.ts
+++ shoppingApp/src/app/recipes/recipe.service.ts
@@ -1,3 +1,16 @@
+import {Recipe} from './recipe.model';
+
 export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple test description',
+      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
+    new Recipe('A Test Recipe2', 'Test description 2',
+      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();   // Exact copy of recipes array instead of accesing directly to recipes
+  }
 
 }
