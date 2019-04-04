f2fa27b04b416d67b25f2ac61d4d5e3b5b43d3f9
lepirus <lepirus@gmail.com>
2019-04-04 12:59:05 +0200

    11.130 Setting up Child (Nested) Routes. Add child routes to users list

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -19,8 +19,9 @@ const appRoutes: Routes = [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent }
       ]},
-  { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UserComponent }
+  { path: 'users', component: UsersComponent , children: [
+      { path: ':id/:name', component: UserComponent }
+      ]}
 ];
 
 @NgModule({
