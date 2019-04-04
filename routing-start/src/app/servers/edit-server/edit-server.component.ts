7b05e0c56c587f8350cfa3112cb83aa86f46f08e
lepirus <lepirus@gmail.com>
2019-04-04 13:31:13 +0200

    11.131 Using Query Parameters - Practice

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) { }
@@ -22,7 +23,11 @@ export class EditServerComponent implements OnInit {
     console.log('fragment: ' + this.route.snapshot.fragment);
 
     // Second way to retrieve query parameters and fragments. See next chapter
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    );
     this.route.fragment.subscribe();
 
     this.server = this.serversService.getServer(1);
--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -11,7 +11,7 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];   // + -> to get a number not a string
@@ -22,6 +22,11 @@ export class ServerComponent implements OnInit {
         this.server = this.serversService.getServer(+params['id']);
       }
     );
+  }
+
+  onEdit() {
+    // this.router.navigate(['/servers', this.server.id, 'edit']);      // ABSOLUTE PATH
+    this.router.navigate(['edit'], {relativeTo: this.route});  // RELATIVE PATH
   }
 
 }
