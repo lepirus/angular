892b2e3080f2a9a4423ac3da5b086c256a2f602c
lepirus <lepirus@gmail.com>
2019-04-05 11:06:53 +0200

    11.140 Controlling Navigation with canDeactivate

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {UsersComponent} from './users/users.component';
 import {UserComponent} from './users/user/user.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {AuthGuardService} from './auth-guard.service';
+import {CanDeactivateGuardService} from './servers/edit-server/can-deactivate-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuardService] }
     ]},
   { path: 'users', component: UsersComponent , children: [
       { path: ':id/:name', component: UserComponent }
