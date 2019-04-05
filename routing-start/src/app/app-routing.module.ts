805ecee294d8044f555251c4e8686056ed11afca
lepirus <lepirus@gmail.com>
2019-04-05 11:50:34 +0200

    11.143 Understanding Location Strategies. Old strategy of routing supported for all browsers

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -32,6 +32,8 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
+    // RouterModule.forRoot(appRoutes, {useHash: true})
+    // Hashtag between like: http://localhost:4200/#/ -> informs to web server to only care about the part after
     RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
