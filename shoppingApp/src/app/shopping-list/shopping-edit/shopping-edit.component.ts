bd89630cf52991855e9c954fab0aba460a6c7273
lepirus <lepirus@gmail.com>
2019-09-05 11:20:07 +0200

    3. Course Project. Update dependencies

--- shoppingApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shoppingApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,8 +8,8 @@ import {ShoppingListService} from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
