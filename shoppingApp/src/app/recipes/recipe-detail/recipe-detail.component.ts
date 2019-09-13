0da9995cd97381737f020aef48a5d9b976e8ad6a
lepirus <lepirus@gmail.com>
2019-09-13 19:50:54 +0200

    12.165 Programmatic Navigation to the Edit Page Navigation for New and Edit Recipe buttons

--- shoppingApp/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ shoppingApp/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,7 +12,8 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     // const id = this.route.snapshot.params['id'];
@@ -26,6 +27,11 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});   // Simple way
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});   // Complex way
   }
 
 }
