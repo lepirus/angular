690a3d7077de21ff2f922b6bbcc75b8d0ec64fcf
lepirus <lepirus@gmail.com>
2019-09-15 00:15:39 +0200

    15.187 TD: Accessing the Form with @ViewChild Fix bug

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('formLogin') signupForm: NgForm;
+  @ViewChild('formLogin', {static: false}) signupForm: NgForm;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
