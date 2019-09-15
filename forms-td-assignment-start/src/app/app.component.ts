b0458b56081d89b87cb8fb5a876cf7793325ded6
lepirus <lepirus@gmail.com>
2019-09-15 02:40:24 +0200

    Assignment 6: Practicing Template-Driven Forms. SOLUTION

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('formLogin', {static: false}) loginForm: NgForm;
+  defaultSubscription = 'advanced';
 
+  onSubmit() {
+    console.log(this.loginForm);
+    console.log(this.loginForm.value);
+  }
 }
