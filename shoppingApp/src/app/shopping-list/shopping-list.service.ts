dcd00ebc9da2f19bd7389af8f0ce1c75355c3fe1
lepirus <lepirus@gmail.com>
2019-09-18 19:47:32 +0200

    14.178 Improving the Reactive Service with Observables (Subjects) Bug fixed

--- shoppingApp/src/app/shopping-list/shopping-list.service.ts
+++ shoppingApp/src/app/shopping-list/shopping-list.service.ts
@@ -25,6 +25,6 @@ export class ShoppingListService {
 
     // Alternative Solution
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 }
