14c298e968e928ba5b98352f67ec169cdb12a352
lepirus <lepirus@gmail.com>
2019-09-19 14:23:09 +0200

    16.228 Adding Ingredient Controls to a Form Array Bug fix latest Angular version

--- shoppingApp/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ shoppingApp/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -59,4 +59,8 @@ export class RecipeEditComponent implements OnInit {
       'ingredients': recipeIngredients
     });
   }
+
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
 }
