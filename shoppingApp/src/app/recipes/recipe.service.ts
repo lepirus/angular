1775a3361261eec530b5696fdce556a1f0f271eb
lepirus <lepirus@gmail.com>
2019-09-22 12:44:50 +0200

    19.281 Transforming Response Data

--- shoppingApp/src/app/recipes/recipe.service.ts
+++ shoppingApp/src/app/recipes/recipe.service.ts
@@ -8,25 +8,26 @@ import { Subject } from 'rxjs/internal/Subject';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Tortilla de Patatas',
-      'A tasty tortilla',
-      'https://upload.wikimedia.org/wikipedia/commons/3/3c/Tortilla_de_patatas_-_2009.jpg',
-      [
-        new Ingredient('Potatoes', 10),
-        new Ingredient('Augnions', 1),
-        new Ingredient('Eggs', 6)
-      ]),
-    new Recipe('Fat Big Burguer',
-      'The Fat Big Burguer as you ever can eat',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1),
-        new Ingredient('Bacon', 2),
-        new Ingredient('Cheese', 2)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Tortilla de Patatas',
+  //     'A tasty tortilla',
+  //     'https://upload.wikimedia.org/wikipedia/commons/3/3c/Tortilla_de_patatas_-_2009.jpg',
+  //     [
+  //       new Ingredient('Potatoes', 10),
+  //       new Ingredient('Augnions', 1),
+  //       new Ingredient('Eggs', 6)
+  //     ]),
+  //   new Recipe('Fat Big Burguer',
+  //     'The Fat Big Burguer as you ever can eat',
+  //     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Bacon', 2),
+  //       new Ingredient('Cheese', 2)
+  //     ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
--- shoppingApp/src/app/shared/data-storage.service.ts
+++ shoppingApp/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -22,6 +23,13 @@ export class DataStorageService {
   fetchRecipes() {
     this.httpClient
       .get<Recipe[]>('https://shopping-app-6c625.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
+        })
+      )
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
