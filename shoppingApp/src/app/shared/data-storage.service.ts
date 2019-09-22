bae828c6a2e9f172566e3f5db8bc2ab5368860cc
lepirus <lepirus@gmail.com>
2019-09-22 12:20:24 +0200

    19.279 Storing Recipes

--- shoppingApp/src/app/shared/data-storage.service.ts
+++ shoppingApp/src/app/shared/data-storage.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.httpClient
+      .put(
+        'https://shopping-app-6c625.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 }
+
