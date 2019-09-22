be5b24202cf13452561c8ab5d50686d4870944a5
lepirus <lepirus@gmail.com>
2019-09-22 11:37:41 +0200

    18.270 Introducing Interceptors

---
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -0,0 +1,18 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+export class AuthInterceptorService implements HttpInterceptor {
+
+  /**
+   * Run code before your request leaves your app, so before it really sent is sent and right before the response
+   * is forwarded to subscribe
+   * @param {HttpRequest<any>} req
+   * @param {HttpHandler} next
+   * @returns {Observable<HttpEvent<any>>}
+   */
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Request is on its way');
+    return next.handle(req);  // let the request continue
+  }
+
+}
