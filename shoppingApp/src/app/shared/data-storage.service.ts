bc8b41d3265e1dee839c77111a4244bb6a16ea84
lepirus <lepirus@gmail.com>
2019-09-29 16:51:23 +0200

    20.300 Attaching the Token with an Interceptor

--- shoppingApp/src/app/shared/data-storage.service.ts
+++ shoppingApp/src/app/shared/data-storage.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
@@ -24,16 +24,10 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.httpClient.get<Recipe[]>(
-          'https://shopping-app-6c625.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          }
-        );
-      }),
+    return this.httpClient.get<Recipe[]>(
+      'https://shopping-app-6c625.firebaseio.com/recipes.json'
+    )
+    .pipe(
       map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
