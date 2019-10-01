2d69c24b2797ae1aa253c7bc29784eb179debea5
lepirus <lepirus@gmail.com>
2019-10-01 17:13:08 +0200

    20.304 Adding an Auth Guard Guard to /recipes redirecting to /auth page

--- shoppingApp/src/app/auth/auth.guard.ts
+++ shoppingApp/src/app/auth/auth.guard.ts
@@ -1,17 +1,21 @@
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { AuthService } from './auth.service';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.user.pipe(map(user => {
       return !!user;    // returns true if user exists, otherwise false
+    }), tap(isAuth => {
+      if (!isAuth) {
+        this.router.navigate(['/auth']);
+      }
     }));
   }
 
