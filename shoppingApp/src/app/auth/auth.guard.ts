a6a75bab68329b51eb9890c43e9ac536d7c2531c
lepirus <lepirus@gmail.com>
2019-10-01 17:08:02 +0200

    20.304 Adding an Auth Guard Guard to deny access to /recipes when user is logged out

---
+++ shoppingApp/src/app/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { AuthService } from './auth.service';
+import { map } from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.user.pipe(map(user => {
+      return !!user;    // returns true if user exists, otherwise false
+    }));
+  }
+
+}
