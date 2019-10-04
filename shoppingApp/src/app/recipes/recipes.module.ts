76e83771414e647742a5032186037a4c771dc65e
lepirus <lepirus@gmail.com>
2019-10-04 12:58:17 +0200

    22.322 Adding Routes to Feature Modules

--- shoppingApp/src/app/recipes/recipes.module.ts
+++ shoppingApp/src/app/recipes/recipes.module.ts
@@ -9,6 +9,7 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { RecipeStartComponent } from './recipe-start/recipe-start.component';
   ],
   imports: [RouterModule,
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RecipesRoutingModule
   ],
   exports: [
     RecipesComponent,
