83f558ccff700254f14f20718b5b51870557c434
lepirus <lepirus@gmail.com>
2019-10-20 13:38:19 +0200

    24.369 Finishing the Login Effect

--- ngrx-01-first-reducer/src/app/auth/store/auth.effects.ts
+++ ngrx-01-first-reducer/src/app/auth/store/auth.effects.ts
@@ -1,11 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { Actions, Effect, ofType } from '@ngrx/effects';
+
+import { of } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import * as AuthActions from './auth.actions';
-import { catchError, map, switchMap } from 'rxjs/operators';
 import { AuthResponseData } from '../auth.service';
 import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
-import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthEffects {
@@ -24,23 +26,41 @@ export class AuthEffects {
         ).pipe(
           map(resData => {
             const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-            return of(new AuthActions.Login({
-                email: resData.email,
-                userId: resData.localId,
-                token: resData.idToken,
-                expirationDate: expirationDate
-              })
-            );
+            return new AuthActions.Login({
+              email: resData.email,
+              userId: resData.localId,
+              token: resData.idToken,
+              expirationDate: expirationDate
+            });
           }),
-          catchError(error => {
-            // ...
-            return of();
+          catchError(errorRes => {
+            let errorMessage = 'An unknown error occurred!';
+            if (!errorRes.error || !errorRes.error.error) {
+              return of(new AuthActions.LoginFail(errorMessage));
+            }
+            switch (errorRes.error.error.message) {
+              case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+                break;
+              case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist.';
+                break;
+              case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct.';
+                break;
+            }
+            return of(new AuthActions.LoginFail(errorMessage));
           })
         );
     })
   );
 
-  constructor(private actions$: Actions, private http: HttpClient) {
+  @Effect({dispatch: false})
+  authSuccess = this.actions$.pipe(ofType(AuthActions.LOGIN), tap(() => {
+    this.router.navigate(['/']);
+  }));
+
+  constructor(private actions$: Actions, private http: HttpClient, private router: Router) {
   }
 
 }
