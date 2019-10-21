cc8472ff633498bab9b41594239057b166232583
lepirus <lepirus@gmail.com>
2019-10-21 18:35:22 +0200

    24.381 Fixing the Auth Redirect

--- ngrx-01-first-reducer/src/app/auth/store/auth.effects.ts
+++ ngrx-01-first-reducer/src/app/auth/store/auth.effects.ts
@@ -28,7 +28,8 @@ const handleAuthentication = (expiresIn: number, email: string, userId: string, 
     email: email,
     userId: userId,
     token: token,
-    expirationDate: expirationDate
+    expirationDate: expirationDate,
+    redirect: true
   });
 };
 
@@ -110,8 +111,10 @@ export class AuthEffects {
   @Effect({dispatch: false})
   authRedirect = this.actions$.pipe(
     ofType(AuthActions.AUTHENTICATE_SUCCESS),
-    tap(() => {
-      this.router.navigate(['/']);
+    tap((authSuccessAction: AuthActions.AuthenticateSuccess) => {
+      if (authSuccessAction.payload.redirect) {
+        this.router.navigate(['/']);
+      }
     }));
 
   @Effect()
@@ -144,7 +147,8 @@ export class AuthEffects {
           email: loadedUser.email,
           userId: loadedUser.id,
           token: loadedUser.token,
-          expirationDate: new Date(userData._tokenExpirationDate)
+          expirationDate: new Date(userData._tokenExpirationDate),
+          redirect: false
         });
         // this.autoLogout(expirationDuration);
       }
