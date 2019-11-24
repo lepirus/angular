04b0dc1a4938495b1c62dce8a3d359f205403e6a
lepirus <lepirus@gmail.com>
2019-11-24 01:49:16 +0100

    28.475 Testing Dependencies: Components and Services

--- testing/src/app/user/user.component.spec.ts
+++ testing/src/app/user/user.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let userService: UserService;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,21 @@ describe('UserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    userService = fixture.debugElement.injector.get(UserService);
+    compiled = fixture.debugElement.nativeElement;
+    // component.isLoggedIn = true;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use the user name from the service', () => {
+    expect(userService.user.name).toEqual(component.user.name);
+  });
+
+  it('should use the user name if user is logged in', () => {
+    expect(compiled.querySelector('p').textContent).not.toContain(component.user.name);
   });
 });
