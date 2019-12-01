38fdb9dbcf1166d4b6c18e1ea122fadf66d1de4d
lepirus <lepirus@gmail.com>
2019-12-01 19:39:09 +0100

    29.481 A First Look At Angular Elements. Normal load

--- angular-elements/src/app/app.module.ts
+++ angular-elements/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
