764f8f0d70e5396365c4dc045ee89747ce5b5381
lepirus <lepirus@gmail.com>
2019-12-25 23:45:22 +0100

    31.492 Setting up a Development Workflow

---
+++ custom-project/webpack.config.common.js
@@ -0,0 +1,26 @@
+var HTMLWebpackPlugin = require('webpack-plugin');
+
+module.exports = {
+    entry: '.src/app/main.ts',
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loaders: ['html-loader']
+            },
+            {
+                test: /\.css$/,
+                loaders: ['raw-loader']
+            }
+        ],
+        exprContextCritical: false
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
