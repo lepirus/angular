4ab890a954a705062e8e54bf01942ee0ff8a9c95
Alberto Gordillo <alberto@asaitec.eu>
2021-06-06 16:57:15 +0200

    Ultima 9. Back to Initial project

--- ultima9/src/app/app.config.component.ts
+++ ultima9/src/app/app.config.component.ts
@@ -120,7 +120,7 @@ export class AppConfigComponent implements OnInit {
 
     themes: any[];
 
-    themeColor = 'purple-cyan';
+    themeColor = 'dark-green';
 
     constructor(public app: AppMainComponent) {}
 
@@ -139,7 +139,6 @@ export class AppConfigComponent implements OnInit {
             {image: 'cyan-amber', label: 'cyan'},
             {image: 'grey-deeporange', label: 'grey'}
         ];
-        this.changeTheme(this.themeColor);
     }
 
     changeTheme(theme) {
