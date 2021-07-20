352dc1d6d792036dcb86b750d62dcc3998f36bcf
Alberto Gordillo <alberto@asaitec.eu>
2021-07-21 00:03:23 +0200

    Databinding - Property Binding

--- Angular11 - Guia Completa/HolaMundo/src/app/app.component.ts
+++ Angular11 - Guia Completa/HolaMundo/src/app/app.component.ts
@@ -6,10 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string = 'Peter';
+  textoPlaceholder: string = 'Escriba algo aqui'
+  deshabilitado: boolean = true;
+  imageSrc: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
 
   constructor() {
-    setInterval(() => this.nombre = 'Jhon', 2000)
+    setInterval(() => this.deshabilitado = false, 2000)
   }
 
   getSuma(numero1: number, numero2: number): number {
